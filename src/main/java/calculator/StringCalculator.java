53a65a264abce78db1d711b8d28e240a9e6ffad7
Atharva Phadke <44662294+atharva8976@users.noreply.github.com>
2021-10-18 10:57:25 +0530

    Update StringCalculator.java

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -11,3 +11,17 @@ public class StringCalculator {
         }
     }
 }
+
+public static final int add(final String numbers) { // Changed void to int
+    String[] numbersArray = numbers.split(",");
+    if (numbersArray.length > 2) {
+        throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
+    } else {
+        for (String number : numbersArray) {
+            if (!number.isEmpty()) {
+                Integer.parseInt(number);
+            }
+        }
+    }
+    return 0; 
+}
